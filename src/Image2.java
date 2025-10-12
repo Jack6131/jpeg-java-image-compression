c701177e21ffc19cf51dbb068907466c6c6b5f16
Jack6131 <soccer61301@gmail.com>
2025-10-11 22:46:01 -0400

    New COde

--- src/Image2.java
+++ src/Image2.java
@@ -1,4 +1,4 @@
- class Image {
+ class Image2 {
 
     double YColorComponent(double R,double G, double B){
         return .2989*R+.5866*G+.1145*B;
@@ -10,7 +10,7 @@
          return 128+.5*R-.4183*G-.0816*B;
      }
 
-     long[] round(double R,double G, double B){
-        return new long [] {Math.round(YColorComponent(R, G, B)),Math.round(C0ColorComponent(R, G, B)),Math.round(CGColorComponent(R, G, B))};
+     int[] round(double R,double G, double B){
+        return new int [] {(int)Math.round(YColorComponent(R, G, B)),(int)Math.round(C0ColorComponent(R, G, B)),(int)Math.round(CGColorComponent(R, G, B))};
      }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try
+        {
+            File ogimage= new File("C:\\Users\\Jack\\Desktop\\ImageCompression\\src\\pngtest.png");
+            BufferedImage im= ImageIO.read(ogimage);
+            ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_PYCC);
+            ColorModel colorModel = new ComponentColorModel(cs, new int[]{8, 8, 8}, false, false,
+                    Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+            int width =im.getWidth();
+            int height = im.getHeight();
+            WritableRaster raster = colorModel.createCompatibleWritableRaster(width,height);
+            BufferedImage ycbcr=new BufferedImage(colorModel,raster,false,null);
+
+            BufferedImage yImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+            BufferedImage cbImage = new BufferedImage(width, height,BufferedImage.TYPE_BYTE_GRAY);
+            BufferedImage crImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+            BufferedImage reconstructedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            Image2 myImage = new Image2();
+
+            for(int x=0;x<width;x++) {
+                for (int y = 0; y < height; y++) {
+                    int rgb = im.getRGB(x,y);
+                    double r = (rgb>>16)& 0xFF;
+                    double g = (rgb>>8)& 0xFF;
+                    double b = rgb & 0xFF;
+                    int[] myChannels = myImage.round(r, g, b);
+                    int channel1=(int)Math.max(0,Math.min(255,(int)myChannels[0]));
+                    int channel2=(int)Math.max(0,Math.min(255,(int)myChannels[1]));
+                    int channel3=(int)Math.max(0,Math.min(255,(int)myChannels[2]));
+                    int yPixel = (channel1 << 16) | (channel1 << 8) | channel1;
+                    int crPixel = (channel2 << 16) | (channel2 << 8) | channel2;
+                    int cbPixel = (channel3 << 16) | (channel3 << 8) | channel3;
+                    int greyScale = (channel1<<16)|(channel2<<8)| channel3;
+
+                    //Conversion Back
+                    double newR = myChannels[0] + 1.402 * (myChannels[2] - 128);
+                    double newG = myChannels[0] - 0.344136 * (myChannels[1] - 128) - 0.714136 * (myChannels[2] - 128);
+                    double newB = myChannels[0] + 1.772 * (myChannels[1] - 128);
+
+
+                    int finalR = Math.max(0, Math.min(255, (int) Math.round(newR)));
+                    int finalG = Math.max(0, Math.min(255, (int) Math.round(newG)));
+                    int finalB = Math.max(0, Math.min(255, (int) Math.round(newB)));
+
+
+                    int finalRgb = (finalR << 16) | (finalG << 8) | finalB;
+                    reconstructedImage.setRGB(x, y, finalRgb);
+                    yImage.setRGB(x, y, yPixel);
+                    cbImage.setRGB(x, y, cbPixel);
+                    crImage.setRGB(x, y, crPixel);
+                    raster.setPixel(x,y,new int[]{yPixel,crPixel,cbPixel});
+                }
+
+            }
+            ImageIO.write(yImage, "png", new File("y_channel.png"));
+            ImageIO.write(cbImage, "png", new File("cb_channel.png"));
+            ImageIO.write(crImage, "png", new File("cr_channel.png"));
+            ImageIO.write(ycbcr, "png", new File("complete.png"));
+            ImageIO.write(reconstructedImage, "jpeg", new File("eww.jpeg"));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
