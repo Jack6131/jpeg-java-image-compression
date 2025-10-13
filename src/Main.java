279c7adf737960597edd23daf78211e455387780
Jack <soccer61301@gmail.com>
2025-10-13 17:47:49 -0400

    Down Sample Function Sample

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,25 @@ import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
 public class Main {
+    //Chroma Down Sample
+    public int [][][] downsample(int height,int width,int [][][]channelMatrix){
+        int [][][] matrix= new int[2][height/2][width/2];
+        for(int y=0;y+1<height;y+=2){
+            for(int x=0;x+1<width;x+=2){
+                int cravg= (channelMatrix[1][y][x]+channelMatrix[1][y+1][x+1]+channelMatrix[1][y+1][x]+channelMatrix[1][y][x+1])/4;
+                int cbavg= (channelMatrix[2][y][x]+channelMatrix[2][y+1][x+1]+channelMatrix[2][y+1][x]+channelMatrix[2][y][x+1])/4;
+                matrix[0][y/2][x/2]=cravg;
+                matrix[1][y/2][x/2]=cbavg;
+            }
+        }
+        return matrix;
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
         try
         {
@@ -24,7 +43,7 @@ public class Main {
             BufferedImage reconstructedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
             Image2 myImage = new Image2();
-
+            int [][][]ChannelMatrix= new int[3][height][width];
             for(int x=0;x<width;x++) {
                 for (int y = 0; y < height; y++) {
                     int rgb = im.getRGB(x,y);
@@ -38,6 +57,10 @@ public class Main {
                     int yPixel = (channel1 << 16) | (channel1 << 8) | channel1;
                     int crPixel = (channel2 << 16) | (channel2 << 8) | channel2;
                     int cbPixel = (channel3 << 16) | (channel3 << 8) | channel3;
+                    ChannelMatrix[0][y][x]=yPixel;
+                    ChannelMatrix[1][y][x]=crPixel;
+                    ChannelMatrix[2][y][x]=cbPixel;
+
                     int greyScale = (channel1<<16)|(channel2<<8)| channel3;
 
                     //Conversion Back
